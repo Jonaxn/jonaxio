62598903d091d37d3442490d7486cfb35e2d6e1c
jonax <jonaxwork@outlook.com>
2023-10-11 15:41:07 +0800

    use mantine form not blitz form

--- src/pages/auth/reset-password.page.tsx
+++ src/pages/auth/reset-password.page.tsx
@@ -1,6 +1,6 @@
 import Layout from "src/core/layouts/Layout";
 import { LabeledTextField } from "src/core/components/LabeledTextField";
-import { Form, FORM_ERROR } from "src/core/components/Form";
+import { FORM_ERROR } from "src/core/components/Form";
 import { ResetPassword } from "@/features/auth/schemas";
 import resetPassword from "@/features/auth/mutations/resetPassword";
 import { BlitzPage, Routes } from "@blitzjs/next";
@@ -8,12 +8,36 @@ import { useRouter } from "next/router";
 import { useMutation } from "@blitzjs/rpc";
 import Link from "next/link";
 import { assert } from "blitz";
+import { Button, PasswordInput, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
 
 const ResetPasswordPage: BlitzPage = () => {
   const router = useRouter();
   const token = router.query.token?.toString();
   const [resetPasswordMutation, { isSuccess }] = useMutation(resetPassword);
+  const form = useForm({
+    initialValues: {
+      password: "",
+      passwordConfirmation: "",
+    },
+  });
 
+  let onSubmit = async (values) => {
+    try {
+      assert(token, "token is required.");
+      await resetPasswordMutation({ ...values, token });
+    } catch (error: any) {
+      if (error.name === "ResetPasswordError") {
+        return {
+          [FORM_ERROR]: error.message,
+        };
+      } else {
+        return {
+          [FORM_ERROR]: "Sorry, we had an unexpected error. Please try again.",
+        };
+      }
+    }
+  };
   return (
     <Layout title="Reset Your Password">
       <div>
@@ -27,38 +51,32 @@ const ResetPasswordPage: BlitzPage = () => {
             </p>
           </div>
         ) : (
-          <Form
-            submitText="Reset Password"
-            schema={ResetPassword}
-            initialValues={{
-              password: "",
-              passwordConfirmation: "",
-              token,
-            }}
-            onSubmit={async (values) => {
-              try {
-                assert(token, "token is required.");
-                await resetPasswordMutation({ ...values, token });
-              } catch (error: any) {
-                if (error.name === "ResetPasswordError") {
-                  return {
-                    [FORM_ERROR]: error.message,
-                  };
-                } else {
-                  return {
-                    [FORM_ERROR]: "Sorry, we had an unexpected error. Please try again.",
-                  };
-                }
-              }
-            }}
-          >
-            <LabeledTextField name="password" label="New Password" type="password" />
-            <LabeledTextField
-              name="passwordConfirmation"
-              label="Confirm New Password"
-              type="password"
+          <form onSubmit={form.onSubmit(onSubmit)}>
+            <PasswordInput withAsterisk label="Password" {...form.getInputProps("password")} />
+            <PasswordInput
+              withAsterisk
+              label="passwordConfirmation"
+              {...form.getInputProps("passwordConfirmation")}
             />
-          </Form>
+            <Button type="submit">Submit</Button>
+          </form>
+          // <Form
+          //   submitText="Reset Password"
+          //   schema={ResetPassword}
+          //   initialValues={{
+          //     password: "",
+          //     passwordConfirmation: "",
+          //     token,
+          //   }}
+          //   onSubmit={onSubmit}
+          // >
+          //   <LabeledTextField name="password" label="New Password" type="password" />
+          //   <LabeledTextField
+          //     name="passwordConfirmation"
+          //     label="Confirm New Password"
+          //     type="password"
+          //   />
+          // </Form>
         )}
       </div>
     </Layout>
--- src/pages/auth/signup/components/SignupForm.tsx
+++ src/pages/auth/signup/components/SignupForm.tsx
@@ -1,8 +1,11 @@
 import { LabeledTextField } from "@/core/components/LabeledTextField";
-import { Form, FORM_ERROR } from "@/core/components/Form";
+import { FORM_ERROR } from "@/core/components/Form";
 import signup from "@/features/auth/mutations/signup";
 import { Signup } from "@/features/auth/schemas";
 import { useMutation } from "@blitzjs/rpc";
+import { Vertical } from "mantine-layout-components";
+import { Button, Title, PasswordInput, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
 
 type SignupFormProps = {
   onSuccess?: () => void;
@@ -10,32 +13,51 @@ type SignupFormProps = {
 
 export const SignupForm = (props: SignupFormProps) => {
   const [signupMutation] = useMutation(signup);
-  return (
-    <div>
-      <h1>Create an Account</h1>
 
-      <Form
-        submitText="Create Account"
-        schema={Signup}
-        initialValues={{ email: "", password: "" }}
-        onSubmit={async (values) => {
-          try {
-            await signupMutation(values);
-            props.onSuccess?.();
-          } catch (error: any) {
-            if (error.code === "P2002" && error.meta?.target?.includes("email")) {
-              // This error comes from Prisma
-              return { email: "This email is already being used" };
-            } else {
-              return { [FORM_ERROR]: error.toString() };
-            }
-          }
-        }}
-      >
-        <LabeledTextField name="email" label="Email" placeholder="Email" />
-        <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
-      </Form>
-    </div>
+  const form = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+    },
+  });
+
+  let onSubmit = async (values: { email: string; password: string } | undefined) => {
+    console.log(values);
+    try {
+      await signupMutation(values);
+      props.onSuccess?.();
+    } catch (error: any) {
+      if (error.code === "P2002" && error.meta?.target?.includes("email")) {
+        // This error comes from Prisma
+        return { email: "This email is already being used" };
+      } else {
+        return { [FORM_ERROR]: error.toString() };
+      }
+    }
+  };
+  return (
+    <Vertical>
+      <Title>Create an Account</Title>
+      <form onSubmit={form.onSubmit(onSubmit)}>
+        <TextInput
+          withAsterisk
+          label="Email"
+          placeholder="your@email.com"
+          {...form.getInputProps("email")}
+        />
+        <PasswordInput
+          withAsterisk
+          label="Password"
+          placeholder="your password"
+          {...form.getInputProps("password")}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Vertical>
   );
 };
 
